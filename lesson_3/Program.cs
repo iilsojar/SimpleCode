ff6807b6e1562dddf3e23d345ae8093494026e7d
Svetlana <iilsojar@gmail.com>
2022-12-08 19:57:29 +0100

    арифметические операции с числами

---
+++ lesson_3/Program.cs
@@ -0,0 +1,9 @@
+﻿//операторы. арифметические операции с числами
+
+int a = 10;
+// int b = 3;
+// int c  = 5;
+int result = a + 5; // = это оператор присваивания //приведение типа в дабл (double)
+Console.WriteLine(result);
+Console.WriteLine();
+
